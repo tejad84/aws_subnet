f2f20ab0d54d62dcfb41023440d42496160438bd
dharmateja <tejad76@gmail.com>
2022-09-15 08:51:28 +0530

    update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-output "name" { #The name of the subnet
-  value = module.csh_conventions.name
-}
+# output "name" { #The name of the subnet
+#   value = aws_subnet.subnet.name
+# }
 output "id" { #The ID of the subnet
   value = aws_subnet.subnet.id
 }
@@ -15,4 +15,4 @@ output "owner_id" { #The ID of the AWS account that owns the subnet.
 }
 output "tags_all" { #A map of tags assigned to the resource, including those inherited from the provider default_tags configuration block.
   value = aws_subnet.subnet.tags_all
-} 
+}
